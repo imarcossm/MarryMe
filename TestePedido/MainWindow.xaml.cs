7658935a127add1fee5aa47bd00cd8dfb0b19af5
imarcossm <viniciusmorais.com@gmail.com>
2024-05-21 09:06:07 -0300

    Primeiro commit

---
+++ TestePedido/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MarryMe
+{
+    public partial class MainWindow : Window
+    {
+        private Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ela disse SIM! 🎉💍");
+        }
+
+        private void NoButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Mover o botão "Não" para uma nova posição aleatória
+            double newX = random.Next(0, (int)(MainGrid.ActualWidth - NoButton.Width));
+            double newY = random.Next(0, (int)(MainGrid.ActualHeight - NoButton.Height));
+
+            NoButton.Margin = new Thickness(newX, newY, 0, 0);
+        }
+    }
+}
